657083885c16041829e3aabd5a679b87198d6a8d
susumu.yata <susumu.yata@gmail.com>
2012-12-25 12:11:24 +0900

    Add marisa2::Utility.

---
+++ lib/marisa2/utility.cc
@@ -0,0 +1,10 @@
+#include "utility.h"
+#include "version.h"
+
+namespace marisa2 {
+
+const char *Utility::version() {
+  return MARISA2_VERSION;
+}
+
+}  // namespace marisa2
---
+++ lib/marisa2/utility.h
@@ -0,0 +1,16 @@
+#ifndef MARISA2_UTILITY_H
+#define MARISA2_UTILITY_H
+
+namespace marisa2 {
+
+class Utility {
+ public:
+  Utility() = delete;
+  ~Utility() = delete;
+
+  static const char *version();
+};
+
+}  // namespace marisa2
+
+#endif  // MARISA2_UTILITY_H
