df146f004e1ec5caefe7261e36fa44d2dc24f8f9
susumu.yata <susumu.yata@gmail.com>
2012-12-27 18:03:24 +0900

    Add marisa2::grimoire::Vector.

---
+++ lib/marisa2/grimoire/vector.cc
@@ -0,0 +1,101 @@
+#include <cstring>
+#include <limits>
+#include <new>
+
+#include "vector.h"
+
+namespace marisa2 {
+namespace grimoire {
+
+Error VectorImpl::map(Mapper &mapper, std::size_t obj_size,
+                      std::size_t num_objs) {
+  if (obj_size == 0) {
+    return MARISA2_ERROR(MARISA2_RANGE_ERROR,
+                         "failed to map vector: obj_size == 0");
+  } else if (num_objs > (std::numeric_limits<std::size_t>::max() / obj_size)) {
+    return MARISA2_ERROR(MARISA2_RANGE_ERROR,
+                         "failed to map vector: too many objects");
+  }
+
+  const char *objs;
+  Error error = mapper.map(&objs, obj_size * num_objs);
+  if (error) {
+    return error;
+  }
+
+  address_ = const_cast<char *>(objs);
+  size_ = num_objs;
+  capacity_ = num_objs;
+  buf_.reset();
+  return MARISA2_SUCCESS;
+}
+
+Error VectorImpl::read(Reader &reader, std::size_t obj_size,
+                       std::size_t num_objs) {
+  if (obj_size == 0) {
+    return MARISA2_ERROR(MARISA2_RANGE_ERROR,
+                         "failed to read vector: obj_size == 0");
+  } else if (num_objs > (std::numeric_limits<std::size_t>::max() / obj_size)) {
+    return MARISA2_ERROR(MARISA2_RANGE_ERROR,
+                         "failed to read vector: too many objects");
+  }
+
+  std::unique_ptr<char[]> new_buf(
+      new (std::nothrow) char[obj_size * num_objs]);
+  if (!new_buf) {
+    return MARISA2_ERROR(MARISA2_MEMORY_ERROR,
+                         "failed to read vector: new char[] failed");
+  }
+  Error error = reader.read(new_buf.get(), obj_size * num_objs);
+  if (error) {
+    return error;
+  }
+
+  address_ = new_buf.get();
+  size_ = num_objs;
+  capacity_ = num_objs;
+  buf_ = std::move(new_buf);
+  return MARISA2_SUCCESS;
+}
+
+Error VectorImpl::reserve(std::size_t obj_size, std::size_t num_objs) {
+  if (num_objs <= capacity_) {
+    return MARISA2_SUCCESS;
+  }
+
+  std::size_t new_capacity = num_objs;
+  if (capacity_ > (num_objs / 2)) {
+    if (capacity_ > (std::numeric_limits<std::size_t>::max() / 2)) {
+      new_capacity = std::numeric_limits<std::size_t>::max();
+    } else {
+      new_capacity = capacity_ * 2;
+    }
+  }
+  return reallocate(obj_size, new_capacity);
+}
+
+Error VectorImpl::reallocate(std::size_t obj_size, std::size_t num_objs) {
+  if (obj_size == 0) {
+    return MARISA2_ERROR(MARISA2_RANGE_ERROR,
+                         "failed to reallocate vector: obj_size == 0");
+  } else if (num_objs > (std::numeric_limits<std::size_t>::max() / obj_size)) {
+    return MARISA2_ERROR(MARISA2_RANGE_ERROR,
+                         "failed to reallocate vector: too many objects");
+  }
+
+  std::unique_ptr<char[]> new_buf(
+      new (std::nothrow) char[obj_size * num_objs]);
+  if (!new_buf) {
+    return MARISA2_ERROR(MARISA2_MEMORY_ERROR,
+                         "failed to reallocate vector: new char[] failed");
+  }
+
+  std::memcpy(new_buf.get(), address_, obj_size * size_);
+  address_ = new_buf.get();
+  capacity_ = num_objs;
+  buf_ = std::move(new_buf);
+  return MARISA2_SUCCESS;
+}
+
+}  // namespace grimoire
+}  // namespace marisa2
---
+++ lib/marisa2/grimoire/vector.h
@@ -0,0 +1,183 @@
+#ifndef MARISA2_GRIMOIRE_VECTOR_H
+#define MARISA2_GRIMOIRE_VECTOR_H
+
+#include <type_traits>
+
+#include "mapper.h"
+#include "reader.h"
+#include "writer.h"
+
+namespace marisa2 {
+namespace grimoire {
+
+class MARISA2_DLL_EXPORT VectorImpl {
+ public:
+  VectorImpl() noexcept : address_(nullptr), size_(0), capacity_(0), buf_() {}
+  ~VectorImpl() noexcept = default;
+
+  VectorImpl(const VectorImpl &) = delete;
+  VectorImpl &operator=(const VectorImpl &) = delete;
+
+  Error map(Mapper &mapper, std::size_t obj_size,
+            std::size_t num_objs) noexcept;
+  Error read(Reader &reader, std::size_t obj_size,
+             std::size_t num_objs) noexcept;
+
+  Error reserve(std::size_t obj_size, std::size_t num_objs) noexcept;
+  Error reallocate(std::size_t obj_size, std::size_t num_objs) noexcept;
+
+  const void *address() const noexcept {
+    return address_;
+  }
+  void *address() noexcept {
+    return address_;
+  }
+
+  std::size_t size() const noexcept {
+    return size_;
+  }
+  std::size_t capacity() const noexcept {
+    return capacity_;
+  }
+
+  void set_size(std::size_t size) noexcept {
+    size_ = size;
+  }
+
+ private:
+  void *address_;
+  std::size_t size_;
+  std::size_t capacity_;
+  std::unique_ptr<char[]> buf_;
+};
+
+template <typename T>
+class Vector {
+  static_assert(std::is_pod<T>::value, "T is not a POD type.");
+
+ public:
+  Vector() noexcept = default;
+  ~Vector() noexcept = default;
+
+  Vector(const Vector &) = delete;
+  Vector &operator=(const Vector &) = delete;
+
+  explicit operator bool() const noexcept {
+    return impl_.size() != 0;
+  }
+
+  Error map(Mapper &mapper, std::size_t size) noexcept {
+    return impl_.map(mapper, sizeof(T), size);
+  }
+  Error read(Reader &reader, std::size_t size) noexcept {
+    return impl_.read(reader, sizeof(T), size);
+  }
+  Error write(Writer &writer) const noexcept {
+    return writer.write(static_cast<const T *>(impl_.address()), impl_.size());
+  }
+
+  Error push_back(const T &obj) noexcept {
+    if (impl_.size() == impl_.capacity()) {
+      Error error = impl_.reserve(sizeof(T), impl_.size() + 1);
+      if (error) {
+        return error;
+      }
+    }
+    static_cast<T *>(impl_.address())[impl_.size()] = obj;
+    impl_.set_size(impl_.size() + 1);
+    return MARISA2_SUCCESS;
+  }
+
+  Error pop_back() noexcept {
+    if (impl_.size() == 0) {
+      return MARISA2_ERROR(MARISA2_STATE_ERROR,
+                           "failed to pop object: empty");
+    }
+    impl_.set_size(impl_.size() - 1);
+    return MARISA2_SUCCESS;
+  }
+
+  Error resize(std::size_t new_size) noexcept {
+    if (new_size > impl_.capacity()) {
+      Error error = impl_.reserve(sizeof(T), new_size);
+      if (error) {
+        return error;
+      }
+    }
+    impl_.set_size(new_size);
+    return MARISA2_SUCCESS;
+  }
+
+  Error resize(std::size_t new_size, const T &obj) noexcept {
+    if (new_size > impl_.capacity()) {
+      Error error = impl_.reserve(sizeof(T), new_size);
+      if (error) {
+        return error;
+      }
+    }
+    for (std::size_t i = impl_.size(); i < new_size; ++i) {
+      static_cast<T *>(impl_.address())[i] = obj;
+    }
+    impl_.set_size(new_size);
+    return MARISA2_SUCCESS;
+  }
+
+  Error reserve(std::size_t required_capacity) noexcept {
+    return impl_.reserve(sizeof(T), required_capacity);
+  }
+
+  Error shrink() noexcept {
+    if (impl_.size() != impl_.capacity()) {
+      return impl_.reallocate(sizeof(T), impl_.size());
+    }
+    return MARISA2_SUCCESS;
+  }
+
+  const T &operator[](std::size_t i) const noexcept {
+    return static_cast<const T *>(impl_.address())[i];
+  }
+  T &operator[](std::size_t i) noexcept {
+    return static_cast<T *>(impl_.address())[i];
+  }
+
+  const T *begin() const noexcept {
+    return static_cast<const T *>(impl_.address());
+  }
+  const T *end() const noexcept {
+    return static_cast<const T *>(impl_.address()) + impl_.size();
+  }
+  T *begin() noexcept {
+    return static_cast<T *>(impl_.address());
+  }
+  T *end() noexcept {
+    return static_cast<T *>(impl_.address()) + impl_.size();
+  }
+
+  const T &front() const noexcept {
+    return *static_cast<const T *>(impl_.address());
+  }
+  const T &back() const noexcept {
+    return static_cast<const T *>(impl_.address())[impl_.size() - 1];
+  }
+  T &front() noexcept {
+    return *static_cast<T *>(impl_.address());
+  }
+  T &back() noexcept {
+    return static_cast<T *>(impl_.address())[impl_.size() - 1];
+  }
+
+  std::size_t size() const noexcept {
+    return impl_.size();
+  }
+  std::size_t capacity() const noexcept {
+    return impl_.capacity();
+  }
+
+ private:
+  VectorImpl impl_;
+};
+
+}  // namespace grimoire
+}  // namespace marisa2
+
+#endif  // MARISA2_GRIMOIRE_VECTOR_H
